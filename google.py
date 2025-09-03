dd15e706e959be4e8a81c1c7484e84bcc8195c30
Aryan Kumar Singh <aaryankumarsingh3153@gmail.com>
2025-09-03 23:41:22 +0530

    palindrome

--- google.py
+++ google.py
@@ -1,7 +1,6 @@
-name = "Aryan"
-age = 19
-weight = 64.0
+def is_palindrome(s: str) -> bool:
+    s = s.lower()
+    return s == s[::-1]
 
-print("Name:", name)
-print("Age:", age)
-print("Weight:", weight)
+print(is_palindrome("Level"))   # True
+print(is_palindrome("Python"))  # False
